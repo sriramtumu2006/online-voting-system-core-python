f526d6bb8a3f42d999ee9b5655f401a4859a417e
sriramtumu2006 <sriramtumu2006@gmail.com>
2025-10-03 15:06:48 +0530

    Update candidate_service.py

--- online-voting-system-core-python/src/services/candidate_service.py
+++ online-voting-system-core-python/src/services/candidate_service.py
@@ -4,6 +4,6 @@ class CandidateService:
     @staticmethod
     def add_candidate(election_id, name, party="Independent"):
         CandidateDAO.add_candidate(election_id, name, party)
-        
+    @staticmethod   
     def list_candidates(election_id):
         return CandidateDAO.get_candidates_by_election(election_id)
