5a8093f5a796dab46820933ef4580d1110589734
sriramtumu2006 <sriramtumu2006@gmail.com>
2025-10-03 15:46:33 +0530

    Update config.py

--- onlne_voting_system/src/config.py
+++ onlne_voting_system/src/config.py
@@ -2,7 +2,8 @@ import streamlit as st
 from supabase import create_client, Client
 
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
-SUPABASE_KEY = st.secrets["SUPABASE_SERVICE_ROLE_KEY"]
+SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 
 def get_supabase() -> Client:
     return create_client(SUPABASE_URL, SUPABASE_KEY)
+
